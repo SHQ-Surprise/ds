f8c933e96bdcb6a25c574dc35726e8094da86625
Sun Haiqi <hasun@mbp-15uau-154.yjoffice.local>
2019-10-09 17:29:50 +0900

    lab1-3

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -24,6 +27,63 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 	}
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
+
+	counter := 0
+	m := &sync.Mutex{}
+	var wg sync.WaitGroup
+	files := make(chan string, 1)
+	if phase == mapPhase {
+		go func() {
+			for _, file := range mapFiles {
+				files <- file
+			}
+		}()
+	}
+
+	workers := make(chan string, ntasks)
+
+	for {
+		if counter == ntasks {
+			break
+		}
+		select {
+		case workerAddr := <-registerChan:
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				m.Lock()
+				index := counter
+				counter++
+				m.Unlock()
+				if phase == mapPhase {
+					call(workerAddr, "Worker.DoTask", DoTaskArgs{jobName, <-files, phase, index, n_other}, nil)
+				} else {
+					call(workerAddr, "Worker.DoTask", DoTaskArgs{jobName, "", phase, index, n_other}, nil)
+				}
+				workers <- workerAddr
+
+			}()
+
+		case workerAddr := <-workers:
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				m.Lock()
+				index := counter
+				counter++
+				m.Unlock()
+				if phase == mapPhase {
+					call(workerAddr, "Worker.DoTask", DoTaskArgs{jobName, <-files, phase, index, n_other}, nil)
+				} else {
+					call(workerAddr, "Worker.DoTask", DoTaskArgs{jobName, "", phase, index, n_other}, nil)
+				}
+				workers <- workerAddr
+			}()
+		}
+	}
+
+	close(files)
+	wg.Wait()
 
 	// All ntasks tasks have to be scheduled on workers. Once all tasks
 	// have completed successfully, schedule() should return.
